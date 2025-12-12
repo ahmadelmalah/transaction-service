b5fb8dca30a92a0af8f590f1fbc32c1d4831562c
Ahmad Elmalah <ahmedelmalah@hotmail.com>
2025-12-12 19:22:26 +0200

    separating mock data to its own folder

--- app/main.py
+++ app/main.py
@@ -2,40 +2,10 @@ from fastapi import FastAPI, HTTPException, Query
 from app.models.transaction import Transaction
 from app.models.summary import Summary
 from typing import List, Optional
+from app.data.mock_data import TRANSACTIONS
+
 
 app = FastAPI()
-
-TRANSACTIONS = [
-    {
-        "id": 1,
-        "customer_id": 10,
-        "amount": 100.0,
-        "currency": "GBP",
-        "status": "completed",
-    },
-    {
-        "id": 2,
-        "customer_id": 10,
-        "amount": 50.0,
-        "currency": "GBP",
-        "status": "pending",
-    },
-    {
-        "id": 3,
-        "customer_id": 20,
-        "amount": 200.0,
-        "currency": "EUR",
-        "status": "completed",
-    },
-    {"id": 4, "customer_id": 20, "amount": 25.0, "currency": "EUR", "status": "failed"},
-    {
-        "id": 5,
-        "customer_id": 30,
-        "amount": 300.0,
-        "currency": "GBP",
-        "status": "completed",
-    },
-]
 
 
 @app.get("/")
